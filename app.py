12b0a51b87c192f1bb2d82d44d6538d30f6c1b38
YashC1308 <f20201683@goa.bits-pilani.ac.in>
2021-08-16 13:52:47 +0530

    fixes

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app = Flask(__name__)
 app.secret_key = 'your secret key'
 
 
-cnx = mysql.connector.connect(user='root', password='15w60ps',
+cnx = mysql.connector.connect(user='root', password='Kuchnahi#00',
                               host='localhost',
                               database='dims')
 cursor = cnx.cursor()
@@ -67,9 +67,6 @@ def Load_Dashboard(party1_Id):
         data.append(temp)
     print(data)
     return data
-
-
-
 
 
 @app.route('/MakeContract', methods=['GET', 'POST'])
@@ -237,7 +234,7 @@ def profile():
         cursor.execute(
             'select type_of_art from profiles group by type_of_art;')
         options = cursor.fetchall()
-        return render_template('profile_page.html', profile=data, options=options)
+        return render_template('profile_page.html', profile=data, options=options, data=[1, 2, 3, 4, 5])
     else:
         return render_template('login.html', msg='Login here')
 
